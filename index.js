c4afb220936980d657f8ef1800e5c99bae780b05
gutchom <yskz.tg@gmail.com>
2017-12-14 03:41:10 +0900

    HTML内のJSを外部ファイルに移動

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script>
-$(function () {
-   /** ページトップ処理 **/
-   // スクロールした場合
-   $(window).scroll(function() {
-       // スクロール位置が200を超えた場合
-       if ($(this).scrollTop() > 200) {
-           $('#pagetop').fadeIn();
-       } else {
-           // ページトップへをフェードアウト
-           $('#pagetop').fadeOut();
-       }
-   });
+(function ($) {
+  /** ページトップ処理 **/
+  // スクロールした場合
+  $(window).scroll(function() {
+    // スクロール位置が200を超えた場合
+    if ($(this).scrollTop() > 200) {
+      $('#pagetop').fadeIn();
+    } else {
+      // ページトップへをフェードアウト
+      $('#pagetop').fadeOut();
+    }
+  });
 
-   // ページトップクリック
-   $('#pagetop').click(function() {
-       // ページトップへスクロール
-       $('html, body').animate({
-           scrollTop: 0
-       }, 300);
-       return false;
-   });
+  // ページトップクリック
+  $('#pagetop').click(function() {
+    // ページトップへスクロール
+    $('html, body').animate({
+      scrollTop: 0
+    }, 300);
+    return false;
+  });
 })(jQuery);
-</script>
-
-<a id="pagetop" href="#top" class="page_top" style="display: none;">ページTOPへ ▲</a>
